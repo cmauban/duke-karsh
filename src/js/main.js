429e4fc1f3d971fee2e34b2b4845d09600636334
Camille Mauban <cm376@duke.edu>
2019-09-03 11:33:10 -0400

    removed angular

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,3 @@
-//IIFE for Angular module
-;(function(){
-
-  var app = angular.module('', []);
-
-    app.controller('', function($http, $rootScope){
-
-      $http.get('')
-        .then(function(response){
-          $rootScope.items = response.data;
-        });
-
-    }); // end countController
-
-})(); // end iffy
-
 $(document).ready(function() {
 
   $('.link').on('click', function(e){
